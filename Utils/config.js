9fcd4525c4dca033243b846853c1ebccdc2d1199
cheerlamounika <cheerlamounika23@gmail.com>
2025-02-17 21:17:35 -0500

    i have added excelsheet

---
+++ Utils/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    baseURL: "https://anupdamoda.github.io/AceOnlineShoePortal/index.html#",
+    credentials: {
+    firstNameInput: "test",
+    lastNameInput: "user",
+    emailInput: "test@abc.com",
+    contactInput: "1234567890",
+    usernameInput: "Test121",
+    passwordInput: "Test121"
+    }
+    }
+    
